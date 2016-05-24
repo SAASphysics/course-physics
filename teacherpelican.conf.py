be752e3df2c000aa88a6408541209dccb2e89ef5
Mark Betnel <itmeson@gmail.com>
2016-05-24 13:50:37 -0700

    split makefiles and configuration files to produce multiple versions of site

---
+++ teacherpelican.conf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Mark Betnel"
+SITENAME = u"Physics A 2016"
+SITEURL = 'http://markbetnel.com/courses/physics/afall2016-me'
+
+TIMEZONE = 'America/Los_Angeles'
+GOOGLE_ANALYTICS = "UA-20141547-1"
+DEFAULT_LANG='en'
+
+DISQUS_SITENAME = 'betnelcourses'
+COMMENTS_INTRO = "Is something unclear? Leave a comment below:"
+
+DOCUTIL_CSS = True
+
+DEFAULT_PAGINATION = 20 
+DISPLAY_PAGES_ON_MENU = False
+
+TAG_SAVE_AS = 'tag/{slug}.html'
+TAG_URL = 'tag/{slug}.html'    
+
+PLUGIN_PATHS = ['/home/mark/Sites/pelican-plugins'] 
+PLUGINS = ['create_calendar', 'gallery', 'tipue_search', 'sitemap', 'latex']
+
+DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+
+SITEMAP = { 'format': 'xml'}
+
+LANDING_PAGE_ABOUT = {'details': """<div itemscope itemtype="http://schema.org/Person"><p>This is the the course website for Fall 2016 Physics A at Seattle Academy.<p></div>"""}
+
+MATH_JAX = {'color':'blue', 'menuSettings': {"zoom": "Click"}}
+
+HYPOTHESIS = True
+
+# Uncommment this to put future dated items in drafts
+#WITH_FUTURE_DATES = False
+
